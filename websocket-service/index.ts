d22d1adc44b19a7e3b53992ef99067ac4ba7b4f3
DevJoelan <joelan.hansen@gmail.com>
2024-03-21 22:41:23 +1100

    boilerplate websocket service

--- websocket-service/index.ts
+++ websocket-service/index.ts
@@ -1,1 +1,19 @@
-console.log("TEST");
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
+
+const app = express();
+const server = http.createServer(app);
+const websocketService = new WebSocket.Server({ server });
+
+websocketService.on('connection', (webSocket) => {
+    webSocket.on('message', (message) => {
+        console.log(`Received message => ${message}`);
+        webSocket.send(`Hello, you sent => ${message}`);
+    });
+    webSocket.send('Hi there, I am a WebSocket server');
+});
+
+server.listen(8999, () => {
+    console.log(`Server started on http://localhost:8999`);
+});
